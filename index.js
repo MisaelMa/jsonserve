84d2baef37fd2a9c80ada3bea3fa3a0a5e7f44d1
MisaelMa <amisael.amir.misael@gmail.com>
2023-06-13 18:03:25 -0500

    fe

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const jsonServer = require('json-server');
-const main = jsonServer.create();
+const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
+server.use(middlewares)
+// Add this before server.use(router)
+server.use(jsonServer.rewriter({
+    '/api/*': '/$1',
+    '/blog/:resource/:id/show': '/:resource/:id'
+}))
+server.use(router)
+server.listen(3000, () => {
+    console.log('JSON Server is running')
+})
 
-main.use(middlewares);
-main.use(router);
-
-main.listen(port);
+// Export the Server API
+module.exports = server
